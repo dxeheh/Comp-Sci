cdf214773eba4a9d3df2088bb36489328009ffaa
hehe xd <ryangearll@gmail.com>
2017-09-06 12:46:17 -0500

    Add files via upload

---
+++ Homework/Lesson 2/TestRectangle.java
@@ -0,0 +1,8 @@
+public class TestRectangle {
+  public static void main(String[] args) { 
+    Rectangle a = new Rectangle();
+    Rectangle b = new Rectangle(4, 40);
+    Rectangle c = new Rectangle(3.5, 35.9);
+    System.out.println(c);
+  }
+}
