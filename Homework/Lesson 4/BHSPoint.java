19dbb2b8a17b0aa8b13cbbc5da551715034a421b
ryan <ryangearll@gmail.com>
2017-09-12 13:19:15 -0500

    Add files via upload

---
+++ Homework/Lesson 4/BHSPoint.java
@@ -0,0 +1,21 @@
+public class BHSPoint{
+ private int myX;
+ private int myY;
+ public BHSPoint(){
+  myX=0;
+  myY = 0;
+ }
+ public BHSPoint(int x, int y){
+  myX = x;
+  myY = y;
+ }
+ public double getDistance(BHSPoint p){
+  return getDistance(p.myX, p.myY);
+ }
+ public double getDistance(int x, int y){
+  return Math.hypot((myX-x), (myY-y));
+ }
+ public String toString(){
+  return String.format("Location : (%d,%d)", myX, myY);
+ }
+}
