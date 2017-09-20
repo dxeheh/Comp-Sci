3ebe64788a381539759616b4aba55ee24fbb6c7f
ryan <ryangearll@gmail.com>
2017-09-20 12:36:14 -0500

    Add files via upload

--- Homework/Lesson 5/BHSInteger.java
+++ Homework/Lesson 5/BHSInteger.java
@@ -1,13 +1,17 @@
+/*---------------
+ * Ryan Earll
+ * Lesson 5, HW 1
+  ---------------*/
 public class BHSInteger {
   int value;
   public BHSInteger(int x){value = x;}
-  public int getInteger(){return value;}
-  public String toString(){return ""+value;}
+  public int getInteger(){return new Integer(value);}
+  public String toString(){return ""+getInteger();}
   public String getHexString(){
     if(value<1)return "0";
     String h = "0123456789ABCDEF";
     String r = "";
-    int x = value;
+    int x = getInteger();
     while(x>0){
      int d = x % 16;
      r = h.charAt(d) + r;
@@ -15,9 +19,9 @@ public class BHSInteger {
     }
     return r;
   }
-  public boolean isEven(){return isEven(value);}
-  public boolean isOdd(){return isOdd(value);}
-  public boolean isPrime(){return isPrime(value);}
+  public boolean isEven(){return isEven(getInteger());}
+  public boolean isOdd(){return isOdd(getInteger());}
+  public boolean isPrime(){return isPrime(getInteger());}
   public static boolean isEven(int x){
     if(x%2 == 0)return true;
     return false;
